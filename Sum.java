6e98e1bd09573d3c3fed242adee755c127ccca9b
mayanksolanki5 <mayanksolanki5401@gmail.com>
2022-03-28 18:33:08 +0530

    2nd

--- Sum.java
+++ Sum.java
@@ -2,8 +2,7 @@ public class Sum{
    public static void main(String args[])
    {
          int a=10,b=20;
-         int sum=a+b;
-         System.out.println(sum);
+         System.out.println(a+b);
    }
    
 }
